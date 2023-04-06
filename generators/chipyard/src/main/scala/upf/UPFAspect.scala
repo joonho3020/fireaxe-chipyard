eeb751d9347c73c80b887f6fbad65439a17730c2
Sriram Sridhar <s.sridhar@a5.Millennium.Berkeley.EDU>
2023-04-05 19:42:08 -0700

    UPF Generation [ci skip]

--- generators/chipyard/src/main/scala/upf/UPFAspect.scala
+++ generators/chipyard/src/main/scala/upf/UPFAspect.scala
@@ -4,12 +4,14 @@ package chipyard.upf
 import chisel3.aop.Aspect
 import firrtl.{AnnotationSeq}
 import chipyard.TestHarness
+import freechips.rocketchip.stage.phases.TargetDirKey
 
 import freechips.rocketchip.diplomacy.LazyModule
 
 abstract class UPFAspect[T <: TestHarness](upf: UPFFunc.UPFFunction) extends Aspect[T] {
 
   final override def toAnnotation(top: T): AnnotationSeq = {
+    UPFFunc.UPFPath = top.p(TargetDirKey) + "/upf"
     upf(top.lazyDut)
     AnnotationSeq(Seq()) // noop
   }
@@ -18,4 +20,5 @@ abstract class UPFAspect[T <: TestHarness](upf: UPFFunc.UPFFunction) extends Asp
 
 object UPFFunc {
   type UPFFunction = PartialFunction[LazyModule, Unit]
+  var UPFPath = "" // output dir path
 }
