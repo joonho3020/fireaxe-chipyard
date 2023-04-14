c35f86543390686b7ce3714271fb0e61524a1d69
Mihai Tudor <mihai.tudor@berkeley.edu>
2023-04-13 17:47:36 -0700

    Some updates and implemented custom 0/1/2/3 but still not working

--- librocc/generic.cc
+++ librocc/generic.cc
@@ -13,17 +13,37 @@ REGISTER_EXTENSION(generic, []() { return new generic_t; })
 
 #define dprintf(...) { if (p->get_log_commits_enabled()) printf(__VA_ARGS__); }
 
-reg_t generic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2) {
-  dprintf("ROCC: encountered unknown instruction with funct: %d\n", insn.funct);
+reg_t generic_t::custom0(rocc_insn_t insn, reg_t xs1, reg_t xs2) {
+  printf("ROCC: encountered instruction custom0 with funct: %d\n", insn.funct);
   return 0;
 }
 
+reg_t generic_t::custom1(rocc_insn_t insn, reg_t xs1, reg_t xs2) {
+  printf("ROCC: encountered instruction custom1 with funct: %d\n", insn.funct);
+  return 0;
+}
+
+reg_t generic_t::custom2(rocc_insn_t insn, reg_t xs1, reg_t xs2) {
+  printf("ROCC: encountered instruction custom2 with funct: %d\n", insn.funct);
+  return 0;
+}
+
+reg_t generic_t::custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2) {
+  printf("ROCC: encountered instruction custom3 with funct: %d\n", insn.funct);
+  return 0;
+}
+
+define_custom_func(generic_t, "generic", generic_custom0, custom0)
+define_custom_func(generic_t, "generic", generic_custom1, custom1)
+define_custom_func(generic_t, "generic", generic_custom2, custom2)
 define_custom_func(generic_t, "generic", generic_custom3, custom3)
 
 std::vector<insn_desc_t> generic_t::get_instructions()
 {
   std::vector<insn_desc_t> insns;
-  //printf("Received an instruction to generic accelerator, proceeding to fail :)");
+  push_custom_insn(insns, ROCC_OPCODE3, ROCC_OPCODE_MASK, ILLEGAL_INSN_FUNC, generic_custom0);
+  push_custom_insn(insns, ROCC_OPCODE3, ROCC_OPCODE_MASK, ILLEGAL_INSN_FUNC, generic_custom1);
+  push_custom_insn(insns, ROCC_OPCODE3, ROCC_OPCODE_MASK, ILLEGAL_INSN_FUNC, generic_custom2);
   push_custom_insn(insns, ROCC_OPCODE3, ROCC_OPCODE_MASK, ILLEGAL_INSN_FUNC, generic_custom3);
   return insns;
 }
--- librocc/generic.h
+++ librocc/generic.h
@@ -14,15 +14,18 @@ class generic_t : public extension_t
   public:
     const char* name() { return "generic" ; }
     
+    reg_t custom0(rocc_insn_t insn, reg_t xs1, reg_t xs2);
+    reg_t custom1(rocc_insn_t insn, reg_t xs1, reg_t xs2);
+    reg_t custom2(rocc_insn_t insn, reg_t xs1, reg_t xs2);
     reg_t custom3(rocc_insn_t insn, reg_t xs1, reg_t xs2);
 
     virtual std::vector<insn_desc_t> get_instructions();
     virtual std::vector<disasm_insn_t*> get_disasms();
     
-    void reset();
+    void reset() {};
 
    protected:
-    //Anything needs to go? Lol what sorts of things need to be protected? Abstracted traits?    
+    //None for now   
 };
 
 #endif
