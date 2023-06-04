e9ca1a8b885b348863a4b9c6a38d2b36d75ef511
joey0320 <joonho0320@gmail.com>
2023-06-04 14:17:28 -0700

    Add partition configs

---
+++ generators/chipyard/src/main/scala/config/PartitionConfigs.scala
@@ -0,0 +1,14 @@
+package chipyard
+
+import org.chipsalliance.cde.config.{Config}
+
+class SBUS16MempressRocketConfig extends Config(
+  new mempress.WithMemPress(singleL2 = false, maxStreams = 4) ++                                    // use Mempress (memory traffic generation) accelerator
+  new chipyard.config.WithSystemBusWidth(128) ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
+class Sha3SlowMemRocketConfig extends Config(
+  new sha3.WithSha3Accel(fastMem = false) ++                                // add SHA3 rocc accelerator
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
