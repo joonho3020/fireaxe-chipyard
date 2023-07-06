8f10ad1041fa6a7754268aff3dd119e79f4b9874
Sagar Karandikar <sagark@eecs.berkeley.edu>
2023-07-06 11:12:04 -0700

    bump boom, update configs

--- generators/chipyard/src/main/scala/config/HyperscaleSoCTopConfigs.scala
+++ generators/chipyard/src/main/scala/config/HyperscaleSoCTopConfigs.scala
@@ -8,73 +8,101 @@ class WithExtMemIdBits(n: Int) extends Config((site, here, up) => {
   case ExtMem => up(ExtMem, site).map(x => x.copy(master = x.master.copy(idBits = n)))
 })
 
-class HyperscaleSoCBaseConfig extends Config(
-  new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=16, capacityKB=2048) ++
-  new freechips.rocketchip.subsystem.WithNBanks(8) ++
+class HyperscaleSoCRocketBaseConfig extends Config(
+  //new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=16, capacityKB=2048) ++
+  //new freechips.rocketchip.subsystem.WithNBanks(8) ++
   new WithExtMemIdBits(7) ++
-  new freechips.rocketchip.subsystem.WithNMemoryChannels(4) ++
+  new freechips.rocketchip.subsystem.WithNMemoryChannels(1) ++
   new Config ((site, here, up) => {
     case SystemBusKey => up(SystemBusKey, site).copy(beatBytes = 32)
   }) ++
-  new RocketConfig)
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
 
-class HyperscaleSoCClientNodeConfig extends Config(
+class HyperscaleSoCRocketClientNodeConfig extends Config(
   new compressacc.AcceleratorPlacementRoCC ++
   new compressacc.WithSnappyCompleteASIC ++
   new protoacc.WithProtoAccelSerOnly ++
   new protoacc.WithProtoAccelDeserOnly ++
-  new HyperscaleSoCBaseConfig)
+  new HyperscaleSoCRocketBaseConfig)
 
-class HyperscaleSoCServerNodeConfig extends Config(
+class HyperscaleSoCRocketServerNodeConfig extends Config(
   new compressacc.AcceleratorPlacementRoCC ++
   new compressacc.WithMergedCompressorLatencyInjection ++
   new compressacc.WithMergedDecompressor16Spec ++
   new protoacc.WithProtoAccelSerOnly ++
   new protoacc.WithProtoAccelDeserOnly ++
-  new HyperscaleSoCBaseConfig)
+  new HyperscaleSoCRocketBaseConfig)
 
-class HyperscaleSoCBaseConfig16MBL2 extends Config(
+
+class HyperscaleSoCMegaBoomBaseConfig extends Config(
+  //new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=16, capacityKB=2048) ++
+  //new freechips.rocketchip.subsystem.WithNBanks(8) ++
+  new WithExtMemIdBits(7) ++
+  new freechips.rocketchip.subsystem.WithNMemoryChannels(1) ++
+  new boom.common.WithNMegaTapeoutBooms(1) ++
+  new Config ((site, here, up) => {
+    case SystemBusKey => up(SystemBusKey, site).copy(beatBytes = 32)
+  }) ++
+  new chipyard.config.AbstractConfig)
+
+class HyperscaleSoCMegaBoomClientNodeConfig extends Config(
+  new compressacc.AcceleratorPlacementRoCC ++
+  new compressacc.WithSnappyCompleteASIC ++
+  new protoacc.WithProtoAccelSerOnly ++
+  new protoacc.WithProtoAccelDeserOnly ++
+  new HyperscaleSoCMegaBoomBaseConfig)
+
+class HyperscaleSoCMegaBoomServerNodeConfig extends Config(
+  new compressacc.AcceleratorPlacementRoCC ++
+  new compressacc.WithMergedCompressorLatencyInjection ++
+  new compressacc.WithMergedDecompressor16Spec ++
+  new protoacc.WithProtoAccelSerOnly ++
+  new protoacc.WithProtoAccelDeserOnly ++
+  new HyperscaleSoCMegaBoomBaseConfig)
+
+class HyperscaleSoCRocketBaseConfig16MBL2 extends Config(
   new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=16, capacityKB=16*1024) ++
-  new HyperscaleSoCBaseConfig)
+  new HyperscaleSoCRocketBaseConfig)
 
-class HyperscaleSoCBaseConfig16MBL2And8MemChan extends Config(
+class HyperscaleSoCRocketBaseConfig16MBL2And8MemChan extends Config(
   new freechips.rocketchip.subsystem.WithNMemoryChannels(8) ++
-  new HyperscaleSoCBaseConfig16MBL2)
+  new HyperscaleSoCRocketBaseConfig16MBL2)
 
 class SnappyBothConfig16MBL2And8MemChanRoCC extends Config(
   new compressacc.AcceleratorPlacementRoCC ++
   new compressacc.WithSnappyCompleteFireSim ++
-  new HyperscaleSoCBaseConfig16MBL2And8MemChan)
+  new HyperscaleSoCRocketBaseConfig16MBL2And8MemChan)
 
 class SnappyBothConfig16MBL2And8MemChanChiplet extends Config(
   new compressacc.AcceleratorPlacementChiplet ++
   new compressacc.WithSnappyCompleteFireSim ++
-  new HyperscaleSoCBaseConfig16MBL2And8MemChan)
+  new HyperscaleSoCRocketBaseConfig16MBL2And8MemChan)
 
 class SnappyBothConfig16MBL2And8MemChanPCIeLocalCache extends Config(
   new compressacc.AcceleratorPlacementPCIeLocalCache ++
   new compressacc.WithSnappyCompleteFireSim ++
-  new HyperscaleSoCBaseConfig16MBL2And8MemChan)
+  new HyperscaleSoCRocketBaseConfig16MBL2And8MemChan)
 
 class SnappyBothConfig16MBL2And8MemChanPCIeNoCache extends Config(
   new compressacc.AcceleratorPlacementPCIeNoCache ++
   new compressacc.WithSnappyCompleteFireSim ++
-  new HyperscaleSoCBaseConfig16MBL2And8MemChan)
+  new HyperscaleSoCRocketBaseConfig16MBL2And8MemChan)
 
 class SnappyDecompressorConfig16MBL2And8MemChanPCIeNoCache extends Config(
   new compressacc.AcceleratorPlacementPCIeNoCache ++
   new compressacc.WithSnappyDecompressor ++
-  new HyperscaleSoCBaseConfig16MBL2And8MemChan)
+  new HyperscaleSoCRocketBaseConfig16MBL2And8MemChan)
 
 class ZstdDecompressorConfig16MBL2And8MemChanSpec16 extends Config(
   new compressacc.WithZstdDecompressor16 ++
-  new HyperscaleSoCBaseConfig16MBL2And8MemChan)
+  new HyperscaleSoCRocketBaseConfig16MBL2And8MemChan)
 
 class ZstdDecompressorConfig16MBL2And8MemChanSpec32 extends Config(
   new compressacc.WithZstdDecompressor32 ++
-  new HyperscaleSoCBaseConfig16MBL2And8MemChan)
+  new HyperscaleSoCRocketBaseConfig16MBL2And8MemChan)
 
 class MergedCompressorLatencyInjectionHyperscaleRocketConfig8MBL2 extends Config(
   new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=16, capacityKB=8*1024) ++
   new compressacc.WithMergedCompressorLatencyInjection ++
-  new HyperscaleSoCBaseConfig)
+  new HyperscaleSoCRocketBaseConfig)
