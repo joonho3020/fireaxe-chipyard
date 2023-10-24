67624f6665d75ef86ad4f8ce742fae1ec876d54a
joey0320 <joonho0320@gmail.com>
2023-10-23 22:43:15 -0700

    Bump icenet | ddio configs

--- generators/chipyard/src/main/scala/config/DDIOConfigs.scala
+++ generators/chipyard/src/main/scala/config/DDIOConfigs.scala
@@ -22,9 +22,23 @@ class DDIODualRocketConfig extends Config(
   new chipyard.config.AbstractConfig)
 
 class DDIOOctaRocketConfig extends Config(
-  new chipyard.harness.WithLoopbackNIC ++                 // drive NIC IOs with loopback
-  new icenet.WithIceNIC ++                                // add an IceNIC
   new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=2, capacityKB=512) ++
   new freechips.rocketchip.subsystem.WithNBanks(2) ++     // 2 L2 banks
   new freechips.rocketchip.subsystem.WithNBigCores(8) ++  // dual rocket-core
   new chipyard.config.AbstractConfig)
+
+class DDIOOctaLoopbackNICRocketConfig extends Config(
+  new chipyard.harness.WithLoopbackNIC ++                 // drive NIC IOs with loopback
+  new icenet.WithIceNIC ++                                // add an IceNIC
+  new chipyard.DDIOOctaRocketConfig)
+
+class DDIOOctaLargeBoomConfig extends Config(
+  new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=2, capacityKB=512) ++
+  new freechips.rocketchip.subsystem.WithNBanks(2) ++     // 2 L2 banks
+  new boom.common.WithNLargeBooms(8) ++
+  new chipyard.config.AbstractConfig)
+
+class DDIOOctaLoopbackNICLargeBoomConfig extends Config(
+  new chipyard.harness.WithLoopbackNIC ++                 // drive NIC IOs with loopback
+  new icenet.WithIceNIC ++                                // add an IceNIC
+  new chipyard.DDIOOctaLargeBoomConfig)
