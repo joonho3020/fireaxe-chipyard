7233eeadcb496737ae2f8210e120896b2c626548
joey0320 <joonho0320@gmail.com>
2023-11-01 21:57:32 -0700

    Update configs

--- generators/chipyard/src/main/scala/config/DDIOConfigs.scala
+++ generators/chipyard/src/main/scala/config/DDIOConfigs.scala
@@ -81,9 +81,9 @@ class DDIODoDecaBoom128kBL2SbusRingNoCConfig extends Config(
         "system[3]" -> 16,
         "pbus" -> 12)), // TSI is on the pbus, so serial-tl and pbus should be on the same node
     NoCParams(
-      topology        = UnidirectionalTorus1D(17),
-      channelParamGen = (a, b) => UserChannelParams(Seq.fill(10) { UserVirtualChannelParams(4) }),
-      routingRelation = NonblockingVirtualSubnetworksRouting(UnidirectionalTorus1DDatelineRouting(), 5, 2))
+      topology        = BidirectionalTorus1D(17),
+      channelParamGen = (a, b) => UserChannelParams(Seq.fill(10) { UserVirtualChannelParams(20) }),
+      routingRelation = NonblockingVirtualSubnetworksRouting(BidirectionalTorus1DShortestRouting(), 5, 2))
   )) ++
   new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=2, capacityKB=128) ++
   new freechips.rocketchip.subsystem.WithNBanks(4) ++     // 2 L2 banks
@@ -91,43 +91,13 @@ class DDIODoDecaBoom128kBL2SbusRingNoCConfig extends Config(
   new boom.common.WithNLargeBooms(1) ++
   new chipyard.config.AbstractConfig)
 
-class DDIODoDecaBoom256kBL2SbusRingNoCConfig extends Config(
-  new constellation.soc.WithSbusNoC(constellation.protocol.TLNoCParams(
-    constellation.protocol.DiplomaticNetworkNodeMapping(
-      inNodeMapping = ListMap((0 until 12).map(idx => s"Core ${idx} " -> idx) :_*) + ("serial-tl" -> 12),
-      outNodeMapping = ListMap(
-        "system[0]" -> 13,
-        "system[1]" -> 14,
-        "system[2]" -> 15,
-        "system[3]" -> 16,
-        "pbus" -> 12)), // TSI is on the pbus, so serial-tl and pbus should be on the same node
-    NoCParams(
-      topology        = UnidirectionalTorus1D(17),
-      channelParamGen = (a, b) => UserChannelParams(Seq.fill(10) { UserVirtualChannelParams(4) }),
-      routingRelation = NonblockingVirtualSubnetworksRouting(UnidirectionalTorus1DDatelineRouting(), 5, 2))
-  )) ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=2, capacityKB=256) ++
-  new freechips.rocketchip.subsystem.WithNBanks(4) ++     // 2 L2 banks
-  new boom.common.WithCloneBoomTiles(11, 0) ++
-  new boom.common.WithNLargeBooms(1) ++
-  new chipyard.config.AbstractConfig)
+class DDIODoDecaBoomLoopbackNICConfig extends Config(
+  new chipyard.harness.WithLoopbackNIC ++                  // drive NIC IOs with loopback
+  new icenet.WithIceNIC ++                                 // add an IceNIC
+  new chipyard.DDIODoDecaBoom128kBL2SbusRingNoCConfig)
 
-class DDIODoDecaBoom512kBL2SbusRingNoCConfig extends Config(
-  new constellation.soc.WithSbusNoC(constellation.protocol.TLNoCParams(
-    constellation.protocol.DiplomaticNetworkNodeMapping(
-      inNodeMapping = ListMap((0 until 12).map(idx => s"Core ${idx} " -> idx) :_*) + ("serial-tl" -> 12),
-      outNodeMapping = ListMap(
-        "system[0]" -> 13,
-        "system[1]" -> 14,
-        "system[2]" -> 15,
-        "system[3]" -> 16,
-        "pbus" -> 12)), // TSI is on the pbus, so serial-tl and pbus should be on the same node
-    NoCParams(
-      topology        = UnidirectionalTorus1D(17),
-      channelParamGen = (a, b) => UserChannelParams(Seq.fill(10) { UserVirtualChannelParams(4) }),
-      routingRelation = NonblockingVirtualSubnetworksRouting(UnidirectionalTorus1DDatelineRouting(), 5, 2))
-  )) ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=2, capacityKB=512) ++
+class DDIODoDecaBoom128kBL2Config extends Config(
+  new freechips.rocketchip.subsystem.WithInclusiveCache(nWays=2, capacityKB=128) ++
   new freechips.rocketchip.subsystem.WithNBanks(4) ++     // 2 L2 banks
   new boom.common.WithCloneBoomTiles(11, 0) ++
   new boom.common.WithNLargeBooms(1) ++
